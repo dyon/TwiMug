a6bef937f8be413c982def8c75752702e06026cc
Dyon <admin@skwow.net>
2012-10-07 19:26:08 +0200

    Remove useless code.

--- ConfigurationService.py
+++ ConfigurationService.py
@@ -14,12 +14,6 @@ class ConfigurationService:
         try:
             self.config.read('config.cfg')
 
-            # Twitter configuration
-            self.twitter['consumer_key'] = self.config.get('Twitter', 'ConsumerKey')
-            self.twitter['consumer_secret'] = self.config.get('Twitter', 'ConsumerSecret')
-            self.twitter['access_token'] = self.config.get('Twitter', 'AccessToken')
-            self.twitter['access_token_secret'] = self.config.get('Twitter', 'AccessTokenSecret')
-
             return True
         except:
             print 'Error while trying to read the configuration file.'
